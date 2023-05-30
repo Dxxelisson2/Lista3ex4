4ff7bb9c9ae4b2cef45eac5ef74828aab29a5523
Dxxelisson2 <130613973+Dxxelisson2@users.noreply.github.com>
2023-05-30 19:11:18 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int valor = 5;
+            
+
+
+            for(int i=1; i <= 10; i++) 
+            {
+                int resultado = valor * i;
+                Console.WriteLine(valor + "x" + i + "=" + resultado);
+            }
+
+           }
+        }
+    }
+
